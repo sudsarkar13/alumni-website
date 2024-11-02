9b4e7dbe50ea2db775cc999e2644a9fa2ec25c8f
SUDEEPTA SARKAR <sudsarkar13@gmail.com>
2024-11-02 23:22:44 +0530

    feat ✨: Add basic application header
    - Added a basic application header component implementation.

---
+++ components/navigation/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Image from 'next/image';
+
+
+const Header: React.FC = () => {
+  return (
+    <main>
+      <div>Header</div>
+    </main>
+  );
+};
+
+export default Header;
