fe5afe9e68de14ab535ebe8b4454e980fd774b3a
SUDEEPTA SARKAR <sudsarkar13@gmail.com>
2024-11-03 01:00:53 +0530

    refactor ♻️: extract importable variables
    - Changed the variable links to be importable and exported.

--- components/navigation/Nav.tsx
+++ components/navigation/Nav.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-const links = [
+export const links = [
 	{
 		name: "Home",
 		path: "/",
