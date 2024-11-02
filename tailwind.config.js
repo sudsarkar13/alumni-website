a4d55359360c8c503b914da08f14f4e47a44f5cb
SUDEEPTA SARKAR <sudsarkar13@gmail.com>
2024-11-02 10:43:53 +0530

    feat ✨: Integrate marquee animation feature into Tailwind CSS
    - Integrated animation feature marquee including default horizontal and custom vertical animation styles into Tailwind CSS.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import TailwindcssAnimate from 'tailwindcss-animate'
+
 module.exports = {
     darkMode: ['class'],
     content: [
@@ -81,13 +83,31 @@ module.exports = {
   				to: {
   					height: '0'
   				}
+  			},
+  			marquee: {
+  				from: {
+  					transform: 'translateX(0)'
+  				},
+  				to: {
+  					transform: 'translateX(calc(-100% - var(--gap)))'
+  				}
+  			},
+  			'marquee-vertical': {
+  				from: {
+  					transform: 'translateY(0)'
+  				},
+  				to: {
+  					transform: 'translateY(calc(-100% - var(--gap)))'
+  				}
   			}
   		},
   		animation: {
   			'accordion-down': 'accordion-down 0.2s ease-out',
-  			'accordion-up': 'accordion-up 0.2s ease-out'
+  			'accordion-up': 'accordion-up 0.2s ease-out',
+  			marquee: 'marquee var(--duration) infinite linear',
+  			'marquee-vertical': 'marquee-vertical var(--duration) linear infinite'
   		}
   	}
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [TailwindcssAnimate],
 }
