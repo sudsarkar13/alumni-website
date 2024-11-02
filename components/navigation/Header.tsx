cbc25a5ef14a3d07b69f85d7337c5b3f441f235e
SUDEEPTA SARKAR <sudsarkar13@gmail.com>
2024-11-03 00:48:40 +0530

    feat ✨: Modernized Header component with customizable layout
    - Overhauled Header component with a fully customizable modern layout integrated with functional parts such as image imports replaced for items within Header part that added Link function for easy redirect different pages new integrated functionality such it  user items inside mobile UI/also now will be incorporated shimmer different functionality making optional show/hide element of design

--- components/navigation/Header.tsx
+++ components/navigation/Header.tsx
@@ -1,13 +1,39 @@
-import React from 'react';
-import Image from 'next/image';
-
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import ShimmerButton from "@/components/ui/shimmer-button";
+import Nav from "./Nav";
+import { ThemeToggle } from "@/components/themes/theme-toggle";
 
 const Header: React.FC = () => {
-  return (
-    <main>
-      <div>Header</div>
-    </main>
-  );
+	return (
+		<main>
+			<header className={`py-8 xl:py-12 text-black dark:text-white`}>
+				<div className={`container mx-auto flex justify-between`}>
+					{/* logo */}
+					<Link href="/">
+						<h1 className={`text-4xl font-semibold`}>
+							Logo<span className={`text-[#FFD700]`}>.</span>
+						</h1>
+					</Link>
+					{/* desktop nav */}
+					<div className={`hidden lg:flex items-center gap-8`}>
+						<Nav />
+						<Link href="#">
+							<ShimmerButton className={`shadow-2xl`}>
+								<span className="whitespace-pre-wrap text-center font-medium leading-none tracking-tight text-white">
+									Join your peers
+								</span>
+							</ShimmerButton>
+						</Link>
+						<ThemeToggle />
+					</div>
+					{/* mobile nav */}
+				</div>
+			</header>
+		</main>
+	);
 };
 
 export default Header;
