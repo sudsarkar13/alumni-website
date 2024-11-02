0230567906e7662d3f8cd728a0ce4a07657435b3
SUDEEPTA SARKAR <sudsarkar13@gmail.com>
2024-11-02 21:04:25 +0530

    feat ✨: Enhance Tailwind CSS with additional components and animations.
    - Extended and enhanced Tailwind CSS configurations with additional components, keyframe animations, and variability based properties and improved movement methods animations variety utility designs.

--- tailwind.config.js
+++ tailwind.config.js
@@ -65,7 +65,12 @@ module.exports = {
   				'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
   				border: 'hsl(var(--sidebar-border))',
   				ring: 'hsl(var(--sidebar-ring))'
-  			}
+  			},
+  			'color-1': 'hsl(var(--color-1))',
+  			'color-2': 'hsl(var(--color-2))',
+  			'color-3': 'hsl(var(--color-3))',
+  			'color-4': 'hsl(var(--color-4))',
+  			'color-5': 'hsl(var(--color-5))'
   		},
   		keyframes: {
   			'accordion-down': {
@@ -157,6 +162,14 @@ module.exports = {
   				to: {
   					backgroundPosition: 'var(--bg-size) 0'
   				}
+  			},
+  			rainbow: {
+  				'0%': {
+  					'background-position': '0%'
+  				},
+  				'100%': {
+  					'background-position': '200%'
+  				}
   			}
   		},
   		animation: {
@@ -170,7 +183,8 @@ module.exports = {
   			meteor: 'meteor 5s linear infinite',
   			'background-position-spin': 'background-position-spin 3000ms infinite alternate',
   			'shiny-text': 'shiny-text 8s infinite',
-  			gradient: 'gradient 8s linear infinite'
+  			gradient: 'gradient 8s linear infinite',
+  			rainbow: 'rainbow var(--speed, 2s) infinite linear'
   		}
   	}
   },
