0e089f788ff2a9e2e515163082ce04b832bae4da
SUDEEPTA SARKAR <sudsarkar13@gmail.com>
2024-11-03 00:48:47 +0530

    feat ✨: Add Mobile Navigation Component
    - New MobileNav Component added with a simple mobile navigation interface.

---
+++ components/navigation/MobileNav.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from 'react';
+
+const MobileNav: React.FC = () => {
+  return (
+    <main>
+      <div>MobileNav</div>
+    </main>
+  );
+};
+
+export default MobileNav;
