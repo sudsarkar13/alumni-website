a30c32414b3959d06227e27412d8b2401125dc8e
SUDEEPTA SARKAR <sudsarkar13@gmail.com>
2024-11-16 01:29:12 +0530

    feat ✨: Implement responsive navigation closing
    - Added responsive closing functionality for navigation sheet to sync opening and closing links.

--- components/navigation/MobileNav.tsx
+++ components/navigation/MobileNav.tsx
@@ -10,14 +10,16 @@ import {
 } from "@/components/ui/sheet";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
-import { MenuIcon, XIcon } from "lucide-react";
+import { MenuIcon } from "lucide-react";
 import { links } from "./Nav";
 import ShimmerButton from "@/components/ui/shimmer-button";
 
 const MobileNav: React.FC = () => {
 	const pathname = usePathname();
+	const [open, setOpen] = React.useState(false);
+
 	return (
-		<Sheet>
+		<Sheet open={open} onOpenChange={setOpen}>
 			<SheetTrigger className={`flex justify-center items-center`}>
 				<MenuIcon className={`text-[32px] text-primary`} />
 			</SheetTrigger>
@@ -26,7 +28,7 @@ const MobileNav: React.FC = () => {
 				<SheetDescription></SheetDescription>
 				{/* logo */}
 				<div className={`mt-10 mb-16 text-center`}>
-					<Link href={`/`}>
+					<Link href={`/`} onClick={() => setOpen(false)}>
 						<h1 className={`text-2xl font-semibold`}>
 							Logo<span className={`text-[#FFD700]`}>.</span>
 						</h1>
@@ -38,6 +40,7 @@ const MobileNav: React.FC = () => {
 							<Link
 								href={link.path}
 								key={index}
+								onClick={() => setOpen(false)}
 								className={`${
 									link.path === pathname &&
 									"text-yellow-500 dark:text-[#00ff99] border-b-2 border-[#FFD700] dark:border-[#00ff99]"
@@ -46,7 +49,7 @@ const MobileNav: React.FC = () => {
 							</Link>
 						);
 					})}
-					<Link href="#">
+					<Link href="#" onClick={() => setOpen(false)}>
 						<ShimmerButton className={`shadow-2xl`}>
 							<span className="whitespace-pre-wrap text-center font-medium leading-none tracking-tight text-white">
 								Join your peers
